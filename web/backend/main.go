01cebf2fe3cdddf5eede6ad13d7bb1af9d9a1d75
wuzhongyang <b40750428@hotmail.com>
2017-08-15 09:12:28 +0800

    配置seelog

--- web/backend/main.go
+++ web/backend/main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
-	"assets/bitmex"
-	"assets/tushare"
+	ts "assets/web/backend/tushare"
 
+	seelog "github.com/cihub/seelog"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// 配置seelog
+	defer seelog.Flush()
+	logger, err := seelog.LoggerFromConfigAsFile("./conf/seelog.xml")
+	if err != nil {
+		seelog.Critical("err parsing config log file", err)
+		return
+	}
+	seelog.ReplaceLogger(logger)
+
 	r := gin.Default()
-	r.GET("/ping", tushare.Ping)
-	r.GET("/announcement", bitmex.NewAnnouncementAPI().GetList)
-	r.GET("/apikey", bitmex.NewAPIKeyAPI().GetList)
-	r.GET("/spider", bitmex.NewHistoryAPI().SpiderHistory)
-	r.GET("/history", bitmex.NewHistoryAPI().GetHistoryList)
+	// r.GET("/announcement", bitmex.NewAnnouncementAPI().GetList)
+	r.GET("/bank/getShibor/:year", ts.NewBankAPI().GetShibor)
+	r.POST("/deal/getKData/:code", ts.NewDealAPI().GetKData)
 	r.Run(":8000")
 }
